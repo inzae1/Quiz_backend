7625529850934ebb9c1b9bf5b4e32936e5a013b0
inzae1 <dlswho19@gmail.com>
2022-04-18 01:17:58 +0900

    first commit

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,2 +1,13 @@
 from rest_framework import serializers
 from .models import Quiz
+
+
+class QuizSerializer(serializers.ModelSerializer):
+    """
+    model data 를 json type 으로 변경시켜서
+    api 를 통해 통신이 가능하게 만들어주는 클래스
+    """
+
+    class Meta:
+        model = Quiz
+        field = ('title', 'body', 'answer')
